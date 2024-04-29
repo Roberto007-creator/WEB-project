fbc391eb741fff103a391ca34e3bfbd7560466cc
Robert <roberttrenkle@yandex.ru>
2024-04-29 21:47:06 +0300

    Улучшен вывод записей: авторизованному пользователю выводятся только его записи, у личных записей появилась соответствующая метка

--- WEB-проект/main.py
+++ WEB-проект/main.py
@@ -36,8 +36,7 @@ def main():
 def index():
     db_sess = db_session.create_session()
     if current_user.is_authenticated:
-        news = db_sess.query(News).filter(
-            (News.user == current_user) | (News.is_private != True))
+        news = db_sess.query(News).filter(News.user == current_user)
     else:
         news = db_sess.query(News).filter(News.is_private != True)
     return render_template("index.html", news=news)
